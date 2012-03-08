1dacacac2703f967b9600e0edd6396f335fd6bfc
Suresh Koleti <skolet@codeaurora.org>
2012-03-08 15:53:48 +0530

    Contacts: Ignore call(hard) key press if detailed contacts view is not loaded
    If call log is having particular contact with more details then
    it will take time to load the detailed view.
    Before loading detailed view if user is trying to press call button (hard key)
    contacts app will crash due to contact URI is not prepared.
    
    Ignore call(hard) key if detailed view is not loaded completely
    for particular contact.
    
    Change-Id: I57ffc7649607b88e120ed3ac64d12d54d3908776
    CRs-Fixed: 335866

--- src/com/android/contacts/CallDetailActivity.java
+++ src/com/android/contacts/CallDetailActivity.java
@@ -331,6 +331,10 @@ public class CallDetailActivity extends Activity implements ProximitySensorAware
                 TelephonyManager tm = (TelephonyManager)
                         getSystemService(Context.TELEPHONY_SERVICE);
                 if (tm.getCallState() == TelephonyManager.CALL_STATE_IDLE) {
+                    if (mNumber == null) {
+                        Log.e(TAG, "Details view is in progress so ignore CALL KEY");
+                        return true;
+                    }
                     Intent callIntent = new Intent(Intent.ACTION_CALL_PRIVILEGED,
                             Uri.fromParts("tel", mNumber, null));
                     startActivity(callIntent);
