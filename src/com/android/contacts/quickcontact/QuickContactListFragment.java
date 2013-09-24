3f25e84fa461115527e8132cd23593b6021a3585
kaiyiz <kaiyiz@codeaurora.org>
2013-09-24 17:54:37 +0800

    Contacts: Check enable state of sub2 when setting listener for sub2
    Check the enable state of sub1 when settings click listener
    for sub2, so if slot1 is absent, the listener for sub2 will
    not be set.
    
    Check the enable state of sub2 when setting click listener for
    sub2.
    
    CRs-Fixed: 548513
    
    Change-Id: I411643829413b62de308e9902c0236143dd7a9c6

--- src/com/android/contacts/quickcontact/QuickContactListFragment.java
+++ src/com/android/contacts/quickcontact/QuickContactListFragment.java
@@ -179,7 +179,7 @@ public class QuickContactListFragment extends Fragment {
                     callButtonSub2.setImageResource(
                             com.android.contacts.common.R.drawable.ic_ab_dialer_holo_dark);
                     callButtonSub2.setTag(action);
-                    if (MoreContactUtils.isMultiSimEnable(MSimConstants.SUB1)) {
+                    if (MoreContactUtils.isMultiSimEnable(MSimConstants.SUB2)) {
                         callButtonSub2.setOnClickListener(mFifthActionClickListener);
                     }
 
