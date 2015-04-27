e9f9c7c414ac1ca1c2c094dc44f21f638dc92d9b
huangjiwen <huangjiwen@pci-suntektech.com>
2015-04-27 16:08:05 +0800

    Contacts: Fix the memory leak.
     - The contact will force close (without any prompt) and exit to main
       screen. Also there is several seconds of black screen and then
       recovered.
     - Root cause: The code which holds the QuickContactActivity object in
       some long term objects of RCS API callback.So if the activity
       finishes, the memory cannot be recovered.
    
    CRs-Fixed: 824039
    (cherry picked from commit ee54859543a3cdab31f9bd093737575939ef01f7)
    
    Change-Id: I69ac548d3f5ebca1823399792ee15510a92acbeb

--- src/com/android/contacts/quickcontact/QuickContactActivity.java
+++ src/com/android/contacts/quickcontact/QuickContactActivity.java
@@ -184,6 +184,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.lang.ref.WeakReference;
 
 /**
  * Mostly translucent {@link Activity} that shows QuickContact dialog. It loads
@@ -2013,12 +2014,14 @@ public class QuickContactActivity extends ContactsActivity {
                     && !RCSUtil.isLocalProfile(mContactData)) {
                 if (mNeverQueryRcsCapability) {
                     mNeverQueryRcsCapability = false;
-                    RCSUtil.updateRCSCapability(QuickContactActivity.this,
-                            mContactData);
+                    RCSUtil.updateRCSCapability(QuickContactActivity.this, mContactData);
                 }
                 if (mNeverQueryRcsPhoto) {
                     mNeverQueryRcsPhoto = false;
-                    RCSUtil.updateContactPhotoViaServer(QuickContactActivity.this, mContactData);
+                    WeakReference<QuickContactActivity> quickRef;
+                    quickRef = new WeakReference<QuickContactActivity>(QuickContactActivity.this);
+                    WeakReference<Contact> contactRef = new WeakReference<Contact>(mContactData);
+                    RCSUtil.updateContactPhotoViaServer(quickRef, contactRef);
                 }
             }
             Trace.endSection();
@@ -2651,16 +2654,18 @@ public class QuickContactActivity extends ContactsActivity {
                 createLauncherShortcutWithContact();
                 return true;
             case R.id.menu_upload_download: {
-                RCSUtil.createLocalProfileBackupRestoreDialog(this, mContactData,
-                    new RestoreFinishedListener() {
-                        public void onRestoreFinished() {
-                            if (QuickContactActivity.this != null
-                                    && !QuickContactActivity.this
-                                            .isFinishing()) {
-                                onNewIntent(getIntent());
+                final WeakReference<QuickContactActivity> quickRef;
+                quickRef = new WeakReference<QuickContactActivity>(QuickContactActivity.this);
+                final WeakReference<Contact> contactRef = new WeakReference<Contact>(mContactData);
+                RCSUtil.createLocalProfileBackupRestoreDialog(this, contactRef.get(),
+                        new RestoreFinishedListener() {
+                            public void onRestoreFinished() {
+                                QuickContactActivity activity = quickRef.get();
+                                if (activity != null && !activity.isFinishing()) {
+                                    activity.onNewIntent(activity.getIntent());
+                                }
                             }
-                        }
-                    }).show();
+                        }).show();
                 return true;
             }
             case R.id.menu_online_business_hall:
